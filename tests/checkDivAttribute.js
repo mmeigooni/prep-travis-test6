941a9caf9c0812b1f1ebebc92e65b306d76bef6c
mmeigooni <mmeigooni@gmail.com>
2017-03-21 19:13:39 -0500

    first commit

---
+++ tests/checkDivAttribute.js
@@ -0,0 +1,9 @@
+describe('getDivAttribute', function() {
+    var d = document.querySelector('.box');
+
+    it('Should be bar', function() {
+        expect(d.getAttribute('foo')).toBe('bar');
+    });
+
+    console.log("TESTING1")
+});
---
+++ tests/checkDivBg.js
@@ -0,0 +1,9 @@
+describe('getDivBg', function() {
+    var d = document.querySelector('.box');
+
+    it('Should be teal', function() {
+        expect(d.style.backgroundColor).toBe('teal');
+    });
+    console.log("TESTING2")
+
+});
